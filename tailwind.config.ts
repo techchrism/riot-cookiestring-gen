c7b925994d4f89c4178bf06044f9076f79704e5d
techchrism <chris@techchrism.me>
2024-01-28 13:56:17 -0800

    add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,16 @@
 import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui'
 
 const config: Config = {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
   ],
-  darkMode: 'class',
+  darkMode: 'media',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
 
 export default config;
